a73fe8814e9f50073c533c8657356674f8a5ce52
20IT022 <88375580+20IT022@users.noreply.github.com>
2021-08-14 10:04:16 +0530

    Add files via upload

---
+++ Packaging_Cupcakes.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Packaging_Cupcakes {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+        for (int tc = 0; tc < T; tc++) {
+            int N = sc.nextInt();
+
+            System.out.println(solve(N));
+        }
+
+        sc.close();
+    }
+
+    static int solve(int N) {
+        return N / 2 + 1;
+    }
+}
