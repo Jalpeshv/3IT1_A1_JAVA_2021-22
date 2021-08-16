8dc9d139db325bb6c6aa6570c9a1f2b9dc869eca
20IT022 <88375580+20IT022@users.noreply.github.com>
2021-08-16 09:35:15 +0530

    Add files via upload

---
+++ Java_Loops_I_h.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Java_Loops_I_h {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        for(int i=1;i<=10;i++)
+        {
+            int sum=N*i;
+            System.out.println(N+" x "+i+" = "+sum);
+        }
+        bufferedReader.close();
+    }
+}
