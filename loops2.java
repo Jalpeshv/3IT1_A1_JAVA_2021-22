c9552414fb7e044206de17c50b9ff6456218813b
rhythm-999 <88377004+rhythm-999@users.noreply.github.com>
2021-08-18 23:30:23 +0530

    Add files via upload

---
+++ loops2.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+        import java.io.*;
+
+class  loops2{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+
+            int c=a;
+            for(int j=0;j<n;j++)
+            {
+                //int c;
+                c+=(int)Math.pow(2, j)*b;
+                System.out.print(c+" ");
+            }
+            System.out.print("\n");
+
+        }
+        in.close();
+    }
+}
